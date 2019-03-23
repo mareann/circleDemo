e5012b21f1eacbc4d694145acfc59418475c6933
Matt <matt@medean.com>
2019-03-23 12:09:24 -0500

    Encapsulated the API in it's own obeject for testing purposes

--- src/server.js
+++ src/server.js
@@ -1,46 +1,19 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-
-import ErrorHandlers from './utils/errorHandlers';
+import { InitAPI } from './api';
 import { log } from './utils/logger';
 
-const app = express();
-
-// Enable support for JSON parsing of body arguments
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
-
-// Hook up our routes
-app.use('/', require('./routes'));
-
-// If any route throws a ValidationError, this middleware will handle it
-app.use(ErrorHandlers.ParameterValidationErrorHandler);
-
+const api = InitAPI();
 const port = 3000;
 
-// Start the server listening
-const server = app.listen(port, () => {
-  log.info(`Demonstration API Running on port ${port}`);
+// listen for http verbs
+const server = api.listen(port, () => {
+  log.info(`Server starting on port ${port}`);
 });
 
-// Handle Errors
-app.use((err, req, res, next) => {
-  // Log the request with error noted
-  log.error('ERROR - %s, %s', req.method, req.url);
-
-  // Log the Error
-  log.error(err.stack);
-
-  // Return 500 error code
-  res.status(500).send({ message: 'Error' });
-});
-
-// Handle 404s
-app.use((req, res, next) => {
-  res.status(404).send({
-    message: "Houston, we've had a problem here",
-    urlRequested: `${req.method} ${req.url}`,
-  });
+/**
+ * Note: any rejections handled here should be investigated and handled directly in the calling code
+ * This is only to warn you during development that something is not setup correctly
+ */
+process.on('unhandledRejection', (error, promise) => {
+  log.error('UnhandledRejection caught by global unhandled Promise Rejection handler');
+  log.error(error);
 });
